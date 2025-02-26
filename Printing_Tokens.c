ea1721a821d2b2933a7f43e817886a32b2d1446c
MusaabTaha <104390876+MusaabTaha@users.noreply.github.com>
2025-02-26 13:23:31 +0100

    Create Printing_Tokens.c

---
+++ Printing_Tokens.c
@@ -0,0 +1,50 @@
+Given a sentence S print each word of the sentence in a new line.
+
+Input Format
+
+The first and only line contains a sentence, .
+
+Constraints
+
+
+Output Format
+
+Print each word of the sentence in a new line.
+
+Sample Input 0
+
+This is C 
+Sample Output 0
+
+This
+is
+C
+
+/*************************************************************************************/
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    char *s;
+    s = malloc(1024 * sizeof(char));
+    scanf("%[^\n]", s);
+    s = realloc(s, strlen(s) + 1);
+    //Write your logic to print the tokens of the sentence here.
+    
+    for(int i=0;i<strlen(s);i++){
+        
+        if(s[i]==' '){
+            
+            printf("\n");
+        } else {
+            
+            printf("%c",s[i]);
+        }
+        
+    }
+    return 0;
+}
